19c125d504e04d545322e1537535be588f53d0fd
ccazacenco <ccazacenco@gmail.com>
2025-07-04 21:20:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from pathlib import Path
 class Config:
     TELEGRAM_TOKEN = os.getenv("7734347521:AAHpCdItWVUwILotW5oWPa1VOG0WGsIyC5M")
     GOOGLE_CREDS_PATH = Path("credentials/service_account.json")
-    DRIVE_ROOT_FOLDER = os.getenv("DRIVE_ROOT_FOLDER")
+    DRIVE_ROOT_FOLDER = os.getenv("18FK5EcYz5hOm3OZiK0shmx0QTRR0fNxw")
