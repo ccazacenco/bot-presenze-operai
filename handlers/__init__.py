8f7d7917a07f77067b0975b1942d1b0205b648c3
ccazacenco <ccazacenco@gmail.com>
2025-07-04 21:56:49 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,10 @@
 from .registration import registration_handler
 from .presence import presence_handler
 from .reports import report_handler
-from .admin import admin_handler
+from .admin import setup_admin_handlers
 
 def setup_handlers(dispatcher):
     dispatcher.add_handler(registration_handler)
     dispatcher.add_handler(presence_handler)
     dispatcher.add_handler(report_handler)
-    dispatcher.add_handler(admin_handler)
+    dispatcher.add_handler(setup_admin_handler)
